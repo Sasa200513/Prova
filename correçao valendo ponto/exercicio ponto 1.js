4aa690c1ae4d2c33ae4a28862427b00944de4e33
Sasa <lunamizuno2005@gmail.com>
2024-10-08 19:35:29 -0300

    Update exercicio ponto 1.js

--- correçao valendo ponto/exercicio ponto 1.js
+++ correçao valendo ponto/exercicio ponto 1.js
@@ -1,37 +1,13 @@
-/*let numeros = [];
+var numeros=[];
 
-while(true) {
-    let numero = parseFloat(prompt('Digite um número'));
-        break}
-        
-    if (isNaN(numero)){
-        
-    }
-        
-    if (!numeros.includes(numero)){ //includes: determina se um conjunto de caracteres pode ser encontrado dentro de outra string, retornando true ou false
+while (true){
+    let numero=parseFloat(prompt('Digite um número'))
+    if (isNaN(numero))
+        break;
+    if (!numeros.includes(numero)){ //includes: determina se o conjunto de caracteres pode ser encontrado dentro da string
         numeros.push(numero);
-        console.log(`Lista atualizada: ${numero}`);
+        console.log(`Lista atualizada: ${numeros}`)
     } else {
-        console.log(`O número ${numero} já está na lista!`);
-    }
-
-console.log(numeros)*/
-
-
-let numeros = [];
-
-while(true) {
-    let numero = parseFloat(prompt('Digite um número'));
-
-    if (isNaN(numero)){
-        break
-    }
-
-    if (!numeros.includes(numero)){ //includes: determina se um conjunto de caracteres pode ser encontrado dentro de outra string, retornando true ou false
-        numeros.push(numero);
-        console.log(`Lista atualizada: ${numeros}`);
-    } else {
-        console.log(`O número ${numero} já está na lista!`);
+        console.log(`O número ${numero} já consta na lista`)
     }
 }
-console.log(numeros)
